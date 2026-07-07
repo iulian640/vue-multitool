bbe3357a7bcf631e74a98377696f7f1bbd918630
iulian640 <iuliantim21@gmail.com>
2026-07-07 13:13:15 +0200

    feat: operator keys with pending operation state

--- src/components/CalculatorCard.vue
+++ src/components/CalculatorCard.vue
@@ -4,6 +4,8 @@ import { ref } from "vue";
 const display = ref("0");
 const previousValue = ref(null);
 const operator = ref(null);
+const digits = ["7", "8", "9", "4", "5", "6", "1", "2", "3", "0"];
+const ops = ["+", "-", "x", "÷"];
 
 function pressDigit(digit) {
   if (display.value === "0") {
@@ -12,12 +14,22 @@ function pressDigit(digit) {
     display.value += digit;
   }
 }
+
+function pressOperator(op) {
+  previousValue.value = display.value;
+  operator.value = op;
+  display.value = "0";
+}
 </script>
 
 <template>
   <section class="card">
     <h2 class="card__title">Calculadora</h2>
     <div class="calc__display">{{ display }}</div>
-    <button @click="pressDigit('7')">7</button>
+    <p>debug: {{ previousValue }} {{ operator }}</p>
+    <button v-for="d in digits" :key="d" @click="pressDigit(d)">{{ d }}</button>
+    <button v-for="op in ops" :key="op" @click="pressOperator(op)">
+      {{ op }}
+    </button>
   </section>
 </template>
